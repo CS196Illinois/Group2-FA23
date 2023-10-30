2b8570fac56b3a250777c9fa23b1679610ac4719
yaswant2403 <yse2@illiois.edu>
2023-10-30 12:30:22 -0500

    Finished profile_screen.dart documentation

--- Project/example_app/lib/groups_screen.dart
+++ Project/example_app/lib/groups_screen.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+/**
+ * The `GroupsScreen` widget is defined as a `StatefulWidget` because it manages mutable state within itself, specifically the state of the future `_future` that holds the data fetched from the Supabase database.
+ * 1. `Data Fetching and State Management`: `GroupsScreen` fetches data from the Supabase database in its `initState` method and stores this data in a `Future`. 
+ *    The state of this `Future` (whether it is still loading, has completed with data, or has completed with an error) determines what is displayed on the screen. 
+ *    This is mutable state that changes over time (as the `Future` completes), and so it needs to be managed within a `StatefulWidget`.
+ * 2. `Lifecycle Methods`: `GroupsScreen` uses the `initState` lifecycle method to initiate the data fetching when the widget is first created. 
+ *    `StatefulWidget` provides lifecycle methods like `initState`, `didUpdateWidget`, and `dispose` that allow you to manage the widget's state and 
+ *    resources over its lifecycle, which is not possible with a `StatelessWidget`.
+
+ * On the other hand, `ProfileScreen` is a `StatelessWidget` because it does not manage any mutable state within itself. 
+ * It simply displays the current user's email address, which is fetched directly from the Supabase instance, and provides a button to sign out. 
+ * There are no internal state changes in `ProfileScreen` that need to be tracked over time, and so a `StatelessWidget` is sufficient FOR THIS EXAMPLE APP.
+
+In summary, the key difference is that `GroupsScreen` manages mutable state (the result of a future) and 
+has a more complex lifecycle (it fetches data when it is first created), while `ProfileScreen` simply displays data based on the current state of the Supabase instance 
+and does not manage any internal state.
+ */
 class GroupsScreen extends StatefulWidget {
   const GroupsScreen({Key? key}) : super(key: key);
 
--- Project/example_app/lib/profile_screen.dart
+++ Project/example_app/lib/profile_screen.dart
@@ -1,26 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+// ProfileScreen widget, which provides UI for the user's profile
+/**
+ * The `ProfileScreen` widget is defined as a `StatelessWidget` because it does not manage any mutable state within itself. 
+ * In Flutter, a `StatelessWidget` is a widget that describes part of the user interface which can depend only 
+ * on the configuration information (the arguments passed to the widget) and the `BuildContext` in which the widget is inflated. 
+ * In the case of `ProfileScreen`, all the data it needs is fetched directly within the `build` method, and it does not need to maintain any mutable state over time. 
+ * The user's email is fetched from the `Supabase` instance, and the UI is built based on this data. 
+ * When the user signs out, the `Supabase` instance handles the state change, and `ProfileScreen` does not need to manage any state related to this action.
+ * If there were any mutable state that needed to be managed within `ProfileScreen` (for example, if there were interactive elements on the screen that change over time)
+ * ,then it would be necessary to convert `ProfileScreen` to a `StatefulWidget`. 
+ * However, since all state management related to user authentication is handled by the `Supabase` instance, and `ProfileScreen` simply reflects the current state of authentication, 
+ * a `StatelessWidget` is sufficient.
+ * 
+ * This is just for THE EXAMPLE APP. You may have to turn this into a `StatefulWidget` widget as you continue building this screen as your matches may change over time.
+ * Look at the groups_screen.dart or home_screen.dart for an example of `StatefulWidget`.
+ */
 class ProfileScreen extends StatelessWidget {
+  // Constructor for the ProfileScreen widget
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Fetching the current user from Supabase
     final user = Supabase.instance.client.auth.currentUser;
+    // Scaffold widget provides a high-level structure for the screen
     return Scaffold(
       appBar: AppBar(
         title: const Text('Profile'),
       ),
+      // Center widget centers its child widget
       body: Center(
+        // Column widget arranges its children in a vertical line
         child: Column(
+          // Aligning children to the center of the main axis
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // Displaying the user's email
             Text('Email: ${user?.email}'),
+            // SizedBox widget provides a box with a specified size
             const SizedBox(height: 20),
+            // ElevatedButton widget provides a Material Design raised button
             ElevatedButton(
+              // Callback function when the button is pressed
               onPressed: () async {
-                await Supabase.instance.client.auth.signOut();
+                // Signing out the user from Supabase
+                await Supabase.instance.client.auth
+                    .signOut(); // takes them back to the login screen
               },
+              // Text widget provides a label for the button
               child: const Text('Sign Out'),
             ),
           ],

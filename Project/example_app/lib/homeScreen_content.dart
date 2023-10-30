1c4c961ba9fa2b4d26e980803a86483c687d0cee
yaswant2403 <yse2@illiois.edu>
2023-10-30 13:15:31 -0500

    Finished home screen documentation

--- Project/example_app/lib/homeScreen_content.dart
+++ Project/example_app/lib/homeScreen_content.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
+// HomeScreenContent is a StatelessWidget because it does not manage any mutable state.
+// It receives data through its constructor and rebuilds whenever the data changes.
+// YOU MAY NEED TO MAKE THIS A STATEFUL WIDGET because you may need to manage mutable states, so just keep that in mind.
 class HomeScreenContent extends StatelessWidget {
+  // Stream of list of maps, each representing an item from the Supabase database.
   final Stream<List<Map<String, dynamic>>> stream;
-
+  // Constructor requires a stream to be passed in so in home_screen, we ensure that we pass in the stream created there.
   const HomeScreenContent({Key? key, required this.stream}) : super(key: key);
 
+  // See Line 50-End of File in groups_screen.dart for the comments regarding this build function
+
+  // The only difference is that we use a Stream Builder instead of a Future Builder because it is designed to listen to a stream of data from the Supabase database,
+  // which allows it to receive and display real-time updates. When the data in the database changes, the stream automatically emits the updated data,
+  // and the StreamBuilder rebuilds its widget tree to reflect the changes. Future Builder only fetches the data once.
+
+  // We don't have to do that in the Groups_Screen because the widget rebuilds itself automatically whenever the user clicks on the Groups Icon in the Navbar.
+  // As such, the Future Builder always fetches the most up to date data.
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<List<Map<String, dynamic>>>(
--- Project/example_app/lib/home_screen.dart
+++ Project/example_app/lib/home_screen.dart
@@ -6,6 +6,18 @@ import 'profile_screen.dart';
 import 'groups_screen.dart';
 import 'homeScreen_content.dart';
 
+/**
+ * `HomeScreen` is a stateful widget that serves as the main screen of the application. It's stateful because it manages mutable state.
+ *    Specifically, the _selectedScreenIndex, which keeps track of the currently selected tab in the BottomNavigationBar, is the mutable state.
+ *    This state determines which screen or content is currently displayed to the user. 
+ *    When a different tab is tapped, _onItemTapped is called, updating the _selectedScreenIndex state and causing the widget to rebuild 
+ *    and display the content of the newly selected tab.
+ * 
+ * It contains a bottom navigation bar to switch between different sections: Profile, Home, and Groups.
+ * The Home screen itself simply displays a list of items fetched from the Supabase database.
+ * 
+ * For the difference between StatefulWidget and StatelessWidget, look at the comments in `profile_screen.dart` and `groups_screen.dart`.
+ */
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -14,20 +26,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  // for displaying the list of items in table
+  // Stream for displaying the list of items from the Supabase database
   late final Stream<List<Map<String, dynamic>>> _stream;
-  // for selecting which screen user clicks on
-  int _selectedScrennIndex = 1;
-  // list of screens
+  // index for selecting which screen the user is currently viewing
+  int _selectedScrennIndex =
+      1; // set to 1 because in our list below, we have the homeScreen in the list[1] position so we want to display the home screen first after user logs in
 
   @override
   void initState() {
     super.initState();
+    // Initializing the stream to fetch data from the 'items' table in the Supabase database
     _stream = Supabase.instance.client.from('items').stream(primaryKey: [
       'id'
     ]).map((maps) => List<Map<String, dynamic>>.from(maps));
   }
 
+  // Function to handle item taps on the BottomNavigationBar
   void _onItemTapped(int index) {
     setState(() {
       _selectedScrennIndex = index;
@@ -36,20 +50,26 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // List of widgets with the first one being the content to be displayed
+    // List of widgets to display based on selected index
+    // First is profiel with the second one being the home screen content to be displayed and finally the groups screen
+    // adding more screens would mean you just create a new `new_screen.dart` file and then add it to this list after importing at the top
     List<Widget> widgetOptions = <Widget>[
       const ProfileScreen(),
-      HomeScreenContent(stream: _stream),
+      HomeScreenContent(stream: _stream), // ensuring we pass in a stream
       const GroupsScreen(),
+      // another screen if necessary
     ];
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Study Buddies'),
       ),
+      // Center widget centers its child widget
       body: Center(
+        // Displaying the widget based on the currently selected index.
         child: widgetOptions.elementAt(_selectedScrennIndex),
       ),
+      // bottom Navigation Bar widget with three icons
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -67,6 +87,8 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
         currentIndex: _selectedScrennIndex,
         selectedItemColor: Colors.amber[800],
+        // callback function so that whenever the user taps one of the icons on our navigation bar,
+        // we show the corresponding screen by rebuilding the widget
         onTap: _onItemTapped,
       ),
     );

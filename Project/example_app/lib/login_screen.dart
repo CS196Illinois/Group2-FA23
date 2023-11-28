d19ea8727b3fe6ce5399889e7eddd1e59b4a76d4
Chahit Jain <chahit.jain07@gmail.com>
2023-11-27 18:03:14 -0600

    Login works

--- Project/example_app/lib/login_screen.dart
+++ Project/example_app/lib/login_screen.dart
@@ -9,9 +9,20 @@ import 'package:supabase_auth_ui/supabase_auth_ui.dart'; // supabase already has
 import 'home_screen.dart';
 
 // LoginScreen widget, which provides UI for user authentication
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   // Constructor for the LoginScreen widget
   const LoginScreen({Key? key}) : super(key: key);
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  // late final Future<List<Map<String, dynamic>>> _future;
+  @override
+  void initState() {
+    super.initState();
+    // Fetching data from the 'users' table in the Supabase database when the widget is initialized
+    
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,27 +55,109 @@ class LoginScreen extends StatelessWidget {
               print('Sign in user: ${response.user}');
             }
           },
+          
           // Callback function when sign-up is complete
-          onSignUpComplete: (response) {
-            // Handle sign up complete
+          onSignUpComplete: (response) async {
             if (response.user != null) {
-              // If user is not null, navigate to the HomeScreen
-              print('Sign up complete: $response');
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) => const HomeScreen()));
+              String major = response.user?.userMetadata?['major'] ?? '';
+              String bio = response.user?.userMetadata?['bio'] ?? '';
+              String name = response.user?.userMetadata?['name'] ?? '';
+              String rawCourses = response.user?.userMetadata?['courses'] ?? '';
+              List<String> courses = rawCourses.split(',').map((course) => course.trim()).toList();
+              print(major);
+              print(rawCourses);
+              print("Hello see if others print");
+              List<String> formattedCourses = courses.map((course) {
+                //TODO formatting of array
+                return course;
+              }).toList();
+              final supabase = Supabase.instance.client;
+              if (supabase != null && response.user?.email != null) {
+                // Update the Supabase database with the user's major and courses
+                final insertResponse = await supabase.from('users').insert({
+                  'email': response.user?.email,
+                  'name': name,
+                  'major': major,
+                  'bio': bio,
+                  'courses': formattedCourses,
+                  'matches': null,
+                  'created_at': response.user?.createdAt,
+                });
+
+                print('Insert response: $insertResponse');
+                
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()),
+                );
+              } else {
+                print('Supabase instance or user email is null');
+              }
             } else {
-              print('Sign in user: ${response.user}');
+              print('Sign up failed');
             }
           },
+// ...
+
+
+
           // List of additional metadata fields for the form
           metadataFields: [
             MetaDataField(
               // Icon for the username field
               prefixIcon: const Icon(Icons.person),
               // Label for the username field
-              label: 'Username',
+              label: 'Name',
               // Key for the metadata field
-              key: 'username',
+              key: 'name',
+              // Validator function for the username field
+              validator: (val) {
+                if (val == null || val.isEmpty) {
+                  return 'Please enter something';
+                }
+                return null;
+              },
+            ),
+
+            MetaDataField(
+              // Icon for the major field
+              prefixIcon: const Icon(Icons.school),
+              // Label for the major field
+              label: 'Major',
+              // Key for the metadata field
+              key: 'major',
+              // Validator function for the major field
+              validator: (val) {
+                if (val == null || val.isEmpty) {
+                  return 'Please enter something';
+                }
+                return null;
+              },
+            ),
+
+            MetaDataField(
+              // Icon for the username field
+              prefixIcon: const Icon(Icons.pentagon),
+              // Label for the username field
+              label: 'Bio',
+              // Key for the metadata field
+              key: 'bio',
+              // Validator function for the username field
+              validator: (val) {
+                if (val == null || val.isEmpty) {
+                  return 'Please enter something';
+                }
+                return null;
+              },
+            ),
+
+            MetaDataField(
+              // Icon for the username field
+              prefixIcon: const Icon(Icons.book_sharp),
+              // Label for the username field
+              label: 'Courses',
+              // Key for the metadata field
+              key: 'courses',
               // Validator function for the username field
               validator: (val) {
                 if (val == null || val.isEmpty) {
